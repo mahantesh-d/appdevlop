b433bea80adc066d8390883b2db86b4758ddfea0
mahantesh-d <mahanteshdhale@gmail.com>
2023-07-23 16:26:05 +0530

    Adding code for flask

--- pyApplication.py
+++ pyApplication.py
@@ -11,3 +11,13 @@ students = [
 @app.get('/students')
 def user_list():
     return{'students': students}
+
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/students')
+# def hello_world():
+#     return{'students': students}
+
+# if __name__ == '__main__':
+#     app.run(debug=True,port=6379)
